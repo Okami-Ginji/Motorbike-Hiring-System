62dc7f995f7976af1b51a2f1d223b769553c38bc
https://github.com/Okami-Ginji <ginokami24@gmail.com>
2024-06-09 08:29:33 +0700

    update 4/6/2024 sửa AccountDAO, các phương thức lấy account đâng bị thiếu một thuộc tính là note

--- src/main/java/com/colorbike/dao/AccessoryDAO.java
+++ src/main/java/com/colorbike/dao/AccessoryDAO.java
@@ -4,10 +4,74 @@
  */
 package com.colorbike.dao;
 
+import com.colorbike.dto.Accessory;
+import com.colorbike.dto.PriceList;
+import com.colorbike.util.DBUtil;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author huypd
  */
 public class AccessoryDAO {
+    private static AccessoryDAO instance;
+    private Connection conn = DBUtil.makeConnection();
+
+    // Cấm new trực tiếp DAO
+    //Chỉ new DAO qua hàm static getInstance() để quản lí được số object/instance đã new - SINGLETON DESIGN PATTERN
+    private AccessoryDAO() {
+    }
+
+    public static AccessoryDAO getInstance() {
+
+        if (instance == null) {
+            instance = new AccessoryDAO();
+        }
+        return instance;
+    }
     
+    //Lấy tất cả các xe máy
+    public List<Accessory> getAll() {
+        List<Accessory> list = new ArrayList<>();
+        PreparedStatement stm;
+        ResultSet rs;
+        try {
+            String sql = "SELECT * FROM [Accessory];";
+            stm = conn.prepareStatement(sql);
+            rs = stm.executeQuery();
+            while (rs.next()) {
+                list.add(new Accessory(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4),rs.getInt(5)));
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(AccountDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+    
+  
+    public void insert(PriceList t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void update(PriceList t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void delete(PriceList t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public static void main(String[] args) {
+        AccessoryDAO dao = getInstance();
+        List<Accessory> list = dao.getAll();
+        for(Accessory x: list){
+            System.out.println(x);
+        }
+    }
 }
